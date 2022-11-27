0901b69917874161d194b797437a938efda60a21
teddyoweh <teddyoweh@gmail.com>
2022-11-27 16:09:42 -0600

    Update App.js

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import resume from 'resume.pdf'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+    <object data=
+    "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210101201653/PDF.pdf" 
+                    width="800" 
+                    height="500"> 
+            </object>
   );
 }
 
