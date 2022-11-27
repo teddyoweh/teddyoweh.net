a44ff7dea55c45c01614d6b1c1423cb7cdfecdb3
teddyoweh <teddyoweh@gmail.com>
2022-11-27 16:36:35 -0600

    analytics functions

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import resume from './resume.pdf'
+import { Analytics } from '@vercel/analytics/react';
+import Tracker from './Tracker';
 function App() {
   return (
+  <>
+
      <div className="App">
     <object data=
     {resume} type="application/pdf" width="100%" height="100%"> 
                   
             </object>
             </div>
+            <Tracker/>
+            <Analytics/>
+            
+            </>
   );
 }
 
---
+++ resume/src/Tracker.js
@@ -0,0 +1,56 @@
+import React, {Component,useState, useEffect,useCallback} from 'react';
+import axios from 'axios'
+import { deviceinfo } from './utility';
+
+export default function Tracker(){
+ const apibase='https://geolocation-db.com/json';
+    const [ipdata, setipData] = useState({});
+    const data = deviceinfo()
+    const [loaded, setloaded] = useState(false)
+ 
+    const getipdata = useCallback(async () => {
+      axios.get('https://ipapi.co/json/').then((res) => {
+        
+ 
+     
+        
+            setipData(res.data)
+          setloaded(true)
+          data['userdata']=res.data
+     console.log(data)
+     axios.post('https://teddyowehapi.herokuapp.com/api/views/addvdata',data).then((res2) => {
+      
+        localStorage.setItem('viewid',JSON.stringify(res2.data))
+
+
+      }
+      
+      )
+    })
+ 
+    },[])
+    useEffect(() => {
+    getipdata()
+ 
+      
+      
+    
+    },[  getipdata])
+    
+     
+        
+        
+        
+ 
+  
+return(
+<>
+ 
+</>
+
+
+)
+
+
+}
+ 
